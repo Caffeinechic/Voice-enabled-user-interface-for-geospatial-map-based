dd7749511d3455ae83f031b20209f5d8ef509c55
Nisar999 <135937349+Nisar999@users.noreply.github.com>
2024-07-25 20:42:59 +0530

    Add files via upload

---
+++ Voice enabled user interface for geospatial map based/geospatial_voice_backend/maps/views.py
@@ -0,0 +1,13 @@
+# maps/views.py
+
+from rest_framework import generics
+from .models import Location
+from .serializers import LocationSerializer
+
+class LocationList(generics.ListCreateAPIView):
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
+
+class LocationDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
